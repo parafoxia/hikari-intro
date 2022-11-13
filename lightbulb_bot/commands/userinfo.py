9a35bf203f838eb598ed7d2fa6be63f290be437b
Nova <novanai.dev@gmail.com>
2022-11-13 11:41:44 +0000

    Update userinfo command (lightbulb)

--- lightbulb_bot/commands/userinfo.py
+++ lightbulb_bot/commands/userinfo.py
@@ -2,52 +2,64 @@ import datetime as dt
 
 import hikari
 import lightbulb
-from lightbulb import commands
 
+from typing import Union
 
 # The options the command will have. This creates a required member
 # option. Validation is handled for you -- Discord won't let you
 # send the command unless it's a valid member. How cool is that?!
-@lightbulb.option("target", "The member to get information about.", hikari.Member)
+@lightbulb.option("target", "The member to get information about.", hikari.User)
+# Disable the slash command from being visible & useable in DMs
+@lightbulb.app_command_permissions(dm_enabled=False)
 # Convert the function into a command
-@lightbulb.command("userinfo", "Get info on a server member.")
+@lightbulb.command("userinfo", "Get info on a server member.", pass_options=True)
 # Define the types of command that this function will implement
-@lightbulb.implements(commands.SlashCommand)
-async def user_info(ctx: lightbulb.context.Context) -> None:
-    target_ = ctx.options.target
-    # Convert the option into a Member object if lightbulb couldn't resolve it automatically
-    target = (
-        target_
-        if isinstance(target_, hikari.Member)
-        else ctx.get_guild().get_member(target_)
-    )
-    if not target:
-        await ctx.respond("That user is not in the server.")
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def user_info(ctx: lightbulb.Context, target: Union[hikari.InteractionMember, hikari.Member, hikari.User]) -> None:
+    # This slash command won't be available in DMs, but the
+    # prefix command will be, so we do some validation
+    if not ctx.guild_id:
+        await ctx.respond("This command may only be user in servers.")
         return
 
-    created_at = int(target.created_at.timestamp())
-    joined_at = int(target.joined_at.timestamp())
-    roles = (await target.fetch_roles())[1:]  # All but @everyone.
+    # Convert the option into a Member object if lightbulb couldn't resolve it automatically.
+    # If an invalid user was passed as the target (with prefix command), lightbulb will raise
+    # the lightbulb.ConverterFailure error. (This example does not handle this problem).
+    member = (
+        target
+        if isinstance(target, (hikari.Member, hikari.InteractionMember))
+        else ctx.bot.cache.get_member(ctx.guild_id, target)
+    )
+    if not member:
+        await ctx.respond("That user is not in this server.")
+        return
+
+    created_at = int(member.created_at.timestamp())
+    joined_at = int(member.joined_at.timestamp())
+
+    # Create a list of roles, formatting them into role mentions.
+    # The @everyone role's ID is the guild's ID, which we filter out.
+    roles = [f"<@&{role}>" for role in member.role_ids if role != ctx.guild_id]
 
     # Function calls can be chained when creating embeds.
     embed = (
         hikari.Embed(
             title="User information",
-            description=f"ID: {target.id}",
+            description=f"ID: `{member.id}`",
             colour=hikari.Colour(0x563275),
             # Doing it like this is important.
             timestamp=dt.datetime.now().astimezone(),
         )
-        .set_author(name="Information")
+        .set_author(name=str(member))
         .set_footer(
             text=f"Requested by {ctx.member.display_name}",
-            icon=ctx.member.avatar_url,
+            icon=ctx.member.display_avatar_url,
         )
-        .set_thumbnail(target.avatar_url)
+        .set_thumbnail(member.display_avatar_url)
         # These are just a number of example fields.
-        .add_field(name="Discriminator", value=target.discriminator, inline=True)
-        .add_field(name="Bot?", value=target.is_bot, inline=True)
-        .add_field(name="No. of roles", value=len(roles), inline=True)
+        .add_field(name="Discriminator", value=member.discriminator, inline=True)
+        .add_field(name="Bot?", value=str(member.is_bot), inline=True)
+        .add_field(name="No. of roles", value=str(len(roles)), inline=True)
         .add_field(
             name="Created on",
             value=f"<t:{created_at}:d> (<t:{created_at}:R>)",
@@ -58,7 +70,7 @@ async def user_info(ctx: lightbulb.context.Context) -> None:
             value=f"<t:{joined_at}:d> (<t:{joined_at}:R>)",
             inline=False,
         )
-        .add_field(name="Roles", value=" | ".join(r.mention for r in roles))
+        .add_field(name="Roles", value=" | ".join(roles) if roles else "No roles")
     )
 
     # To send a message, use ctx.respond. Using kwargs, you can make
@@ -72,4 +84,4 @@ def load(bot: lightbulb.BotApp):
 
 
 def unload(bot: lightbulb.BotApp):
-    bot.remove_command(bot.get_slash_command("userinfo"))
+    bot.remove_command(user_info)
