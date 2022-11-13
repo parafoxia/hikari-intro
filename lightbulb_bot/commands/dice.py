f0680a02c9430fdfb57a8c1e357682d15d92b7b0
Nova <novanai.dev@gmail.com>
2022-11-13 11:46:12 +0000

    Update dice command (lightbulb)

--- lightbulb_bot/commands/dice.py
+++ lightbulb_bot/commands/dice.py
@@ -1,28 +1,28 @@
 import random
 
 import lightbulb
-from lightbulb import commands
 
 
 # These two are the same type, but are optional. We can provide a
 # default value simply by using the `default` kwarg.
 @lightbulb.option("bonus", "A fixed number to add to the total roll.", int, default=0)
-@lightbulb.option("sides", "The number of sides each die will have.", int, default=6)
+@lightbulb.option("sides", "The number of sides each die will have.", int, default=6, max_value=100)
 # The options the command will have. This creates a required int
 # option. Validation is handled for you -- Discord won't let you
-# send the command unless it's a number. How cool is that?!
-@lightbulb.option("number", "The number of dice to roll.", int)
+# send the command unless it's a number, and only if that number
+# is less than or equal to 25. How cool is that?!
+@lightbulb.option("number", "The number of dice to roll.", int, max_value=25)
 # Convert the function into a command
 @lightbulb.command("dice", "Roll one or more dice.")
 # Define the types of command that this function will implement
-@lightbulb.implements(commands.SlashCommand)
-async def dice(ctx: lightbulb.context.Context) -> None:
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def dice(ctx: lightbulb.SlashContext) -> None:
     # Extract the options from the context
     number = ctx.options.number
     sides = ctx.options.sides
     bonus = ctx.options.bonus
 
-    # Option validation
+    # Option validation for prefix commands
     if number > 25:
         await ctx.respond("No more than 25 dice can be rolled at once.")
         return
@@ -48,4 +48,4 @@ def load(bot: lightbulb.BotApp) -> None:
 
 
 def unload(bot: lightbulb.BotApp) -> None:
-    bot.remove_command(bot.get_slash_command("dice"))
+    bot.remove_command(dice)
